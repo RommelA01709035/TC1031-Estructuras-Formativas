417d8ebe0629f9c44771ca8021ef4e86e9a8fc2e
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2023-11-28 16:00:50 -0600

    Update funciones.h

--- 1_Funciones_Iterativas_Recursivas/funciones.h
+++ 1_Funciones_Iterativas_Recursivas/funciones.h
@@ -1,6 +1,26 @@
-/*
- * funciones.h
- *
- *  Created on: 12/08/2023
- *      Author: A01709035 - Rommel Apcheco Hernández
- */
+#include <iostream>
+#include <string>
+
+class Funciones{
+    public:
+        int sumaIterativa(int n){
+            long acum = 0;
+            for(int i = 0;i <= n; i++ ){
+                acum = acum + i;
+            }
+            return acum;
+        }   
+
+        int sumaRecursiva(int n){
+            if (n == 0) {
+		        return 0;
+	        } else {
+		        return n + sumaRecursiva(n - 1);
+	        }
+        }
+
+        int sumaDirecta(int n){
+            return n * (n + 1) / 2;
+        }    
+    
+};
